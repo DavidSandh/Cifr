db443350db0ed8406544a9828515ae1eed65f1f3
Viktor Ekström <mongomoves@gmail.com>
2017-04-06 13:33:46 +0200

    Ändringar i Conversationlist och dess adapter.

--- app/src/main/java/se/mah/ag7406/cifr/ConversationList.java
+++ app/src/main/java/se/mah/ag7406/cifr/ConversationList.java
@@ -2,17 +2,26 @@ package se.mah.ag7406.cifr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class ConversationList extends AppCompatActivity {
-    private CustomRecyclerViewAdapter adapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation_list);
 
-        adapter = (RecyclerView) findViewById(R.layout.activity_conversation_list);
+        recyclerView = (RecyclerView) findViewById(R.id.conversationList);
+        recyclerManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(recyclerManager);
+        //recyclerAdapter = new CustomRecyclerViewAdapter(); //Ska ha en parameter för att skicka vidare datan?/Viktor
+        recyclerView.setAdapter(recyclerAdapter);
+
+
 
 
 
--- app/src/main/java/se/mah/ag7406/cifr/CustomRecyclerViewAdapter.java
+++ app/src/main/java/se/mah/ag7406/cifr/CustomRecyclerViewAdapter.java
@@ -1,20 +1,35 @@
 package se.mah.ag7406.cifr;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Viktor on 2017-04-06.
  */
 
-public class CustomRecyclerViewAdapter extends RecyclerView.Adapter {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder> {
+    private LayoutInflater inflater;
+    //private Data[] data; Den data som ska fylla rutorna, dvs. bilder och användarnamn.
+
+    public CustomRecyclerViewAdapter(Context context/*, Data[] data*/) {
+        this.inflater = LayoutInflater.from(context);
+        //this.data = data;
+    }
+
+    public CustomRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = (View) inflater.inflate(R.layout.conversation_list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
     }
 
@@ -22,4 +37,13 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter {
     public int getItemCount() {
         return 0;
     }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+        public ViewHolder (View conversationItem) {
+            super(conversationItem);
+            view = conversationItem;
+        }
+    }
+
 }
