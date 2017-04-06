31e80c20d4e36307dc185724a3689178a6785267
Jens Andreassen <andreassen.je@gmail.com>
2017-04-06 12:15:32 +0200

    Uppdatering av login skräm

--- app/src/main/java/se/mah/ag7406/cifr/cifrClient/CifrLoginScreen.java
+++ app/src/main/java/se/mah/ag7406/cifr/cifrClient/CifrLoginScreen.java
@@ -1,15 +1,31 @@
 package se.mah.ag7406.cifr.cifrClient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import se.mah.ag7406.cifr.ConversationList;
 import se.mah.ag7406.cifr.R;
 
 public class CifrLoginScreen extends AppCompatActivity {
-
+    //private Controller cont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_cifr_login_screen);
+        //cont = new Controller();
+    }
+
+    public void logIn(View view){
+        Intent intent = new Intent(this, ConversationList.class);
+        //cont.testprint();
+        startActivity(intent);
+
     }
 }
