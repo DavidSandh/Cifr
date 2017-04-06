3c50a075864d5fdc4a7f544a33c3cc47ff4da5b2
Viktor Ekström <mongomoves@gmail.com>
2017-04-06 11:15:05 +0200

    ConversationList, java och xml.

---
+++ app/src/main/java/se/mah/ag7406/cifr/ConversationList.java
@@ -0,0 +1,13 @@
+package se.mah.ag7406.cifr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConversationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation_list);
+    }
+}
