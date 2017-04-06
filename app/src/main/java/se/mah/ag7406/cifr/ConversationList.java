8260de46bec08a1a32254a9f0e3a43cf52cc6a22
Viktor Ekström <mongomoves@gmail.com>
2017-04-06 11:41:04 +0200

    Mer om conversationlist.

--- app/src/main/java/se/mah/ag7406/cifr/ConversationList.java
+++ app/src/main/java/se/mah/ag7406/cifr/ConversationList.java
@@ -2,12 +2,19 @@ package se.mah.ag7406.cifr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class ConversationList extends AppCompatActivity {
+    private CustomRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation_list);
+
+        adapter = (RecyclerView) findViewById(R.layout.activity_conversation_list);
+
+
+
     }
 }
---
+++ app/src/main/java/se/mah/ag7406/cifr/CustomRecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package se.mah.ag7406.cifr;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Viktor on 2017-04-06.
+ */
+
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
