636b844fd36b0ce3419efce06d4feb82778f962e
Viktor Ekström <mongomoves@gmail.com>
2017-05-19 11:12:42 +0200

    Inga mer landskaps-orientering

--- app/src/main/java/se/mah/ag7406/cifr/client/CreateMessagePackage/CreateMessage.java
+++ app/src/main/java/se/mah/ag7406/cifr/client/CreateMessagePackage/CreateMessage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
